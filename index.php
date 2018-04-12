8fea7f43889bcc8d8f2ba90d3878554d3a14917b
hamzazia2005 <hamzazia2005@gmail.com>
2018-04-12 09:20:58 -0700

    html file is updated

--- index.php
+++ index.php
@@ -4,4 +4,66 @@ require "head.php";
 
 ?>
 
-//Write your HTML code below this line.
+<!--Write your HTML code below this line.-->
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data target="#demo" data-slide-to="3"></li>
+    <li data target="#demo" data-slide-to="4"></li>
+    <li data target="#demo" data-slide-to="5"></li>
+    <li data target="#demo" data-slide-to="6"></li>
+    <li data target="#demo" data-slide-to="7"></li>
+    <li data target="#demo" data-slide-to="8"></li>
+    <li data target="#demo" data-slide-to="9"></li>
+    <li data target="#demo" data-slide-to="10"></li>
+    <li data target="#demo" data-slide-to="11"></li>
+    <li data target="#demo" data-slide-to="12"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image2.jpg" alt="image" width="100%" height="790">
+    </div>
+    <div class="carousel-item">
+      <img src="image3.jpg" alt="image" width="100%" height="790">
+    </div>
+    <div class="carousel-item"> 
+      <img src="image5.jpeg" alt="image" width="100%" height="790">
+    </div>
+    <div class="carousel-item"> 
+      <img src="image6.jpeg" alt="image" width="100%" height="790">
+    </div>
+    <div class="carousel-item"> 
+      <img src="image11.jpg" alt="image" width="100%" height="790">
+    </div>
+    <div class="carousel-item"> 
+      <img src="image13.jpg" alt="image" width="100%" height="790">
+    </div>
+    <div class="carousel-item"> 
+      <img src="image14.jpg" alt="image" width="100%" height="790">
+    </div>
+    <div class="carousel-item"> 
+      <img src="image15.jpg" alt="image" width="100%" height="790">
+    </div>
+    <div class="carousel-item"> 
+      <img src="image17.jpg" alt="image" width="100%" height="790">
+    </div>  
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+</body>
+</html>
