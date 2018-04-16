2e742dd26f8b9ef0ac18e0dff0f6e6721e21d597
Shahzad07618 <38255795+Shahzad07618@users.noreply.github.com>
2018-04-16 06:39:37 -0700

    Bump

--- index.php
+++ index.php
@@ -81,3 +81,38 @@ require "head.php";
     <span class="carousel-control-next-icon"></span>
   </a>
 </div>
+<div class="container">
+  <h2>Our Testimonials</h2>
+  <p> Take a look below to learn what our clients are saying about us</p>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="101" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="102"></li>
+      <li data-target="#myCarousel" data-slide-to="103"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="assets/images/image101.jpg" alt="img" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="assets/images/image102.jpg" alt="img" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="assets/images/image103.jpg" alt="img" style="width:100%;">
+      </div>
+    </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
