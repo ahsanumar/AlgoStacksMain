001f55893cac3cdc959582f9d9e74f54e55b8328
hamzazia2005 <hamzazia2005@gmail.com>
2018-04-16 06:47:31 -0700

    team module updated

--- head.php
+++ head.php
@@ -5,8 +5,28 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/main.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <style>
+            .overlay{
+                position: absolute;
+                bottom: 0;
+                background: rgb(0,0,0);
+                background: rgba(0,0,0,0.5);
+                color: #f1f1f1;
+                width:  10%;
+                transition: .5s;
+                opacity: 0;
+                color: white;
+                font-size: 20px;
+                padding: 15px;
+                text-align: center;
+            }
+            #onhover:hover .overlay{
+                opacity: 1;
+            }
+        </style>
     </head>
     <body>
--- index.php
+++ index.php
@@ -46,31 +46,31 @@ require "head.php";
     <li data target="#demo" data-slide-to="12"></li>
   </ul>
   <div class="carousel-inner">
-    <div class="carousel-item active">
+    <div class="carousel-item active responsive">
       <img src="assets/images/image2.jpg" alt="image" width="100%" height="735vh">
     </div>
-    <div class="carousel-item">
+    <div class="carousel-item responsive">
       <img src="assets/images/image3.jpg" alt="image" width="100%" height="735vh">
     </div>
-    <div class="carousel-item"> 
+    <div class="carousel-item responsive"> 
       <img src="assets/images/image5.jpeg" alt="image" width="100%" height="735vh">
     </div>
-    <div class="carousel-item"> 
+    <div class="carousel-item responsive"> 
       <img src="assets/images/image6.jpeg" alt="image" width="100%" height="735vh">
     </div>
-    <div class="carousel-item"> 
+    <div class="carousel-item responsive"> 
       <img src="assets/images/image11.jpg" alt="image" width="100%" height="735vh">
     </div>
-    <div class="carousel-item"> 
+    <div class="carousel-item responsive"> 
       <img src="assets/images/image13.jpg" alt="image" width="100%" height="735vh">
     </div>
-    <div class="carousel-item"> 
+    <div class="carousel-item responsive"> 
       <img src="assets/images/image14.jpg" alt="image" width="100%" height="735vh">
     </div>
-    <div class="carousel-item"> 
+    <div class="carousel-item responsive"> 
       <img src="assets/images/image15.jpg" alt="image" width="100%" height="735vh">
     </div>
-    <div class="carousel-item"> 
+    <div class="carousel-item responsive"> 
       <img src="assets/images/image17.jpg" alt="image" width="100%" height="735vh">
     </div>  
   </div>
@@ -80,4 +80,40 @@ require "head.php";
   <a class="carousel-control-next" href="#demo" data-slide="next">
     <span class="carousel-control-next-icon"></span>
   </a>
+</div>
+<div style="height:100vh">
+<header  style="text-align:center; padding:40px; font-size: 40px; ">
+  <p> OUR TEAM</p>
+</header>
+  <div class="row team-row first-team-member">
+    <div class="col-sm-3 col-md-3 col-lg-3">
+      <img src="assets/images/image15.jpg" alt="person" class="circle-image">
+      <p style="padding-top:3%">Jonjo’s Assistant, Guy rode more than 200 winners during a 16-year career as a jump jockey before retiring in April 1999, his biggest win coming...</p>
+    </div>
+    <div class="col-sm-3 col-md-3 col-lg-3">
+      <img src="assets/images/image14.jpg" alt="person" class="circle-image">
+    </div>
+    <div class="col-sm-3 col-md-3 col-lg-3">
+      <img src="assets/images/image17.jpg" alt="person" class="circle-image">
+    </div>
+    <div class="col-sm-3 col-md-3 col-lg-3">
+      <img src="assets/images/image13.jpg" alt="person" class="circle-image">
+    </div>  
+  </div>
+  <div class="row team-row second-team first-team-member">
+  <div class="col-sm-3 col-md-3 col-lg-3">
+      <img src="assets/images/image13.jpg" alt="person" class="circle-image">
+      <p style="padding-top:3%">A nephew of Jonjo, Joe joined the team at Jackdaws Castle in October 2010. Joe, whose father Tom is a greyhound trainer in Castletownroche, is in...</p>
+    </div> 
+    <div class="col-sm-3 col-md-3 col-lg-3">
+      <img src="assets/images/image13.jpg" alt="person" class="circle-image">
+    </div> 
+    <div class="col-sm-3 col-md-3 col-lg-3">
+      <img src="assets/images/image13.jpg" alt="person" class="circle-image">
+    </div> 
+    <div class="col-sm-3 col-md-3 col-lg-3">
+      <img src="assets/images/image13.jpg" alt="person" class="circle-image">
+    </div> 
+  </div>
+
 </div>
